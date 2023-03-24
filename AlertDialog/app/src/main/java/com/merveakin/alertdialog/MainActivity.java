c42be816ba30b83a4bc3558638137dfefb4547f7
Merve Akın <m.akin-2001@hotmail.com>
2023-03-24 18:09:12 +0300

    JavaApp_AlertDialog

---
+++ AlertDialog/app/src/main/java/com/merveakin/alertdialog/MainActivity.java
@@ -0,0 +1,49 @@
+package com.merveakin.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this , "Toast Message" , Toast.LENGTH_LONG).show();
+
+    }
+
+    public void save(View view){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+             //save işlemi yapılacak
+           Toast.makeText(MainActivity.this , "Saved" , Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Toast.makeText(MainActivity.this , "Not saved" , Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
+
+    }
+
+
+
+
+
+}
